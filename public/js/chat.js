d825f0c9b472445deb5cce5e16684774c7af342f
ALIASIFGUJJAR <aliasif3324@gmail.com>
2023-07-26 03:02:58 +0200

    restrict the user to with in a room with .to() method

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,6 +12,10 @@ const $sendLocationButton= document.querySelector('#send-location')
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate= document.querySelector('#location-message-template').innerHTML
 
+//Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
+
+
 
 socket.on('message', (message) => {
     console.log(message)
@@ -68,3 +72,5 @@ $sendLocationButton.addEventListener('click',()=>{
         } )
     })
 })
+
+socket.emit('join',{username, room})
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const path = require('path')
 const http = require('http')
 const express = require('express')
-const socketio = require('socket.io')
+const socketio=require('socket.io')
 const Filter = require('bad-words')
-const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const {generateMessage,generateLocationMessage}= require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -17,14 +17,25 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+   // socket.emit('message', generateMessage('Welcome'))
+  // socket.broadcast.emit('message', generateMessage('A new user has joined'))
+
+    socket.on('join',({username,room})=>{
+        socket.join(room)
+
+        socket.emit('message', generateMessage('Welcome'))
+         socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined`))
+
+
+    })
 
     socket.on('sendMessage', (message, callback) => {
-        const filter = new Filter()
+        const filter = new  Filter()
 
-        if (filter.isProfane(message)) {
-            return callback('Profanity is not allowed!')
+        if(filter.isProfane(message)){
+
+            return callback('Profane is not allowed!')
+
         }
 
         io.emit('message', generateMessage(message))
@@ -39,6 +50,7 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         io.emit('message', generateMessage('A user has left!'))
     })
+
 })
 
 server.listen(port, () => {
