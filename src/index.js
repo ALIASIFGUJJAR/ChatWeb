6701c449d135902a929fc38eaeab4fce16e9a453
ALIASIFGUJJAR <ali.asif@tu-ilmenau.de>
2023-10-30 15:54:35 +0100

    Modified index.js file to add user.js functions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const socketio=require('socket.io')
 const Filter = require('bad-words')
 const {generateMessage,generateLocationMessage}= require('./utils/messages')
+const {addUser,removeUser,getUser,getUserInRoom} = require('/utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -20,12 +21,19 @@ io.on('connection', (socket) => {
    // socket.emit('message', generateMessage('Welcome'))
   // socket.broadcast.emit('message', generateMessage('A new user has joined'))
 
-    socket.on('join',({username,room})=>{
+    socket.on('join',({options,room})=>{
+        const {error,user}= addUser({id: socket.id, ...options})
+
+        if(error){
+            return callback(error)
+        }
+
         socket.join(room)
 
         socket.emit('message', generateMessage('Welcome'))
-         socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined`))
+         socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined`))
 
+         callback()
 
     })
 
@@ -48,7 +56,12 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left!'))
+        const user= removeUser(socket.id)
+        
+        if(user){
+            io.to(user.room).emit('message', generateMessage(`${user.username} has left`))
+
+        }
     })
 
 })
